4b18bc5edc8786f027f89e6a1df03917d6873f5a
Masahiro Kozuka <masa.koz@gmail.com>
2019-12-20 22:44:53 -0500

    use libuv instead of libev.

--- server.c
+++ server.c
@@ -5,11 +5,26 @@
 #include <stdio.h>
 #include <winsock2.h>
 
+#include <uv.h>
+
 #include <rusctp.h>
 
 #define BUFLEN 2048 //Max length of buffer
 
-static void log(const char *line, void *argp)
+struct associations
+{
+    SOCKET sock;
+    rusctp_assoc *assoc;
+};
+
+static rusctp_init_config *config = NULL;
+
+static struct associations *assocs = NULL;
+
+uv_loop_t *loop;
+uv_udp_t udp_socket;
+
+static void my_log(const char *line, void *argp)
 {
     fprintf(stderr, "%s\n", line);
     fflush(stderr);
@@ -20,117 +35,171 @@ void dump_pkt(uint8_t *buf, size_t len)
     int i;
     for (i = 0; i < len; i++)
     {
-        printf("%02X", buf[i]);
+        fprintf(stderr, "%02X", buf[i]);
         if ((i % 16) < 15)
         {
-            printf(", ");
+            fprintf(stderr, ", ");
         }
         else
         {
-            printf("\n");
+            fprintf(stderr, "\n");
         }
     }
-    if ((i % 16) != 15)
-        printf("\n");
+    if ((i % 16) < 15)
+        fprintf(stderr, "\n");
+
+    fflush(stderr);
+}
+
+#if 0
+static void recv_cb(EV_P_ ev_io *w, int revents)
+{
+    struct sockaddr_in raddr_sin;
+    char rbuf[BUFLEN], sbuf[BUFLEN], secret[32];
+
+    fprintf(stderr, "recv_cb\n");
+    fflush(stderr);
+
+    while (1)
+    {
+
+        int slen = sizeof(raddr_sin);
+
+        int recv_len = recvfrom(assocs->sock, rbuf, rbuf_len, 0, (struct sockaddr *)&raddr_sin, &slen);
+        if (recv_len == SOCKET_ERROR)
+        {
+            if (WSAGetLastError() == WSAEWOULDBLOCK)
+            {
+                break;
+            }
+            fprintf(stderr, "recvfrom: %d\n", WSAGetLastError());
+            fflush(stderr);
+            exit(EXIT_FAILURE);
+        }
+
+        fprintf(stderr, "Received packet from %s:%d\n", inet_ntoa(raddr_sin.sin_addr), ntohs(raddr_sin.sin_port));
+        fflush(stderr);
+
+        dump_pkt(rbuf, recv_len);
+
+    }
+}
+#endif
+
+static void on_alloc(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf)
+{
+    buf->base = malloc(suggested_size);
+    buf->len = suggested_size;
+}
+
+void on_send(uv_udp_send_t *req, int status)
+{
+    fprintf(stderr, "Write\n");
+}
+
+void on_recv(uv_udp_t *req, ssize_t nread, const uv_buf_t *rbuf, const struct sockaddr *addr, unsigned flags)
+{
+    if (nread < 0)
+    {
+        fprintf(stderr, "Read error %s\n", uv_err_name(nread));
+        uv_close((uv_handle_t *)req, NULL);
+        free(rbuf->base);
+        return;
+    }
+
+    char sender[17] = {0};
+    uv_ip4_name((const struct sockaddr_in *)addr, sender, 16);
+    fprintf(stderr, "Recv from %s\n", sender);
+
+    dump_pkt(rbuf->base, nread);
+
+    uint16_t src_port = 0, dst_port = 0;
+    uint32_t vtag = 0;
+
+    int rc = rusctp_header_info(rbuf->base, nread, &src_port, &dst_port, &vtag);
+    if (rc < 0)
+    {
+        return;
+    }
+
+    fprintf(stderr, "src_port=%u, dst_port=%u, vtag=%u\n", src_port, dst_port, vtag);
+
+    size_t rbuf_len = nread;
+    size_t rbuf_off = 0;
+
+    uv_buf_t sbuf = uv_buf_init((char *)malloc(BUFLEN), BUFLEN);
+    size_t sbuf_len = BUFLEN;
+    if (sbuf.base == NULL)
+    {
+        return;
+    }
+
+    size_t salen;
+    switch (addr->sa_family)
+    {
+    case AF_INET:
+        salen = sizeof(struct sockaddr_in);
+        break;
+    case AF_INET6:
+        salen = sizeof(struct sockaddr_in6);
+        break;
+    default:
+        return;
+    }
+
+    rusctp_assoc *assoc = NULL;
+    assoc = rusctp_accept(addr, salen, rbuf->base, &rbuf_len, sbuf.base, &sbuf.len, config);
+    if (assoc == NULL)
+    {
+        rbuf_off += rbuf_len;
+        if (sbuf_len > 0)
+        {
+            dump_pkt(sbuf.base, sbuf.len);
+
+            uv_udp_send_t *send_req = malloc(sizeof(uv_udp_send_t));
+            if (send_req == NULL)
+            {
+                return;
+            }
+            uv_udp_send(send_req, &udp_socket, &sbuf, 1, addr, on_send);
+        }
+    }
+    else
+    {
+        printf("Association established.\n");
+        rusctp_assoc_free(assoc);
+    }
+
+    free(rbuf->base);
 }
 
 int main()
 {
-    SOCKET s;
-    struct sockaddr_in server, raddr_sin, laddr_sin;
-    int slen, recv_len;
-    char rbuf[BUFLEN], sbuf[BUFLEN], secret[32];
-    WSADATA wsa;
-    rusctp_init_config *config = NULL;
-
-    rusctp_enable_logging(log, NULL, RUSCTP_LOGLEVEL_TRACE);
-
-    if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
-    {
-        printf("WSAStartup: %d\n", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
+    rusctp_enable_logging(my_log, NULL, RUSCTP_LOGLEVEL_TRACE);
 
     config = rusctp_config_new(10001);
     if (config == NULL)
     {
-        printf("rusctp_config_new\n");
+        fprintf(stderr, "rusctp_config_new\n");
         exit(EXIT_FAILURE);
     }
+
+    char secret[32];
+    memset(secret, 0, sizeof(secret));
+
     rusctp_config_set_secret_key(config, secret, sizeof(secret));
 
-    memset(&laddr_sin, 0, sizeof(laddr_sin));
-    laddr_sin.sin_family = AF_INET;
-    laddr_sin.sin_addr.s_addr = inet_addr("127.0.0.1");
+    loop = uv_default_loop();
 
-    rusctp_config_add_laddr(config, (struct sockaddr *)&laddr_sin, sizeof(laddr_sin));
+    uv_udp_init(loop, &udp_socket);
+    struct sockaddr_in recv_addr;
+    uv_ip4_addr("0.0.0.0", 10009, &recv_addr);
+    uv_udp_bind(&udp_socket, (const struct sockaddr *)&recv_addr, UV_UDP_REUSEADDR);
+    uv_udp_recv_start(&udp_socket, on_alloc, on_recv);
 
-    if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
-    {
-        printf("socket: %d\n", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
+    int rc = uv_run(loop, UV_RUN_DEFAULT);
 
-    server.sin_family = AF_INET;
-    server.sin_addr.s_addr = INADDR_ANY;
-    server.sin_port = htons(10009);
+    rusctp_config_free(config);
 
-    if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
-    {
-        printf("bind: %d\n", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
-
-    memset(secret, 0, sizeof(secret));
-    while (1)
-    {
-        size_t rbuf_len = sizeof(rbuf);
-        size_t rbuf_off = 0;
-        size_t sbuf_len = sizeof(sbuf);
-        uint16_t src_port = 0, dst_port = 0;
-        uint32_t vtag = 0;
-        int rc = 0;
-        rusctp_assoc *assoc = NULL;
-
-        slen = sizeof(raddr_sin);
-        if ((recv_len = recvfrom(s, rbuf, rbuf_len, 0, (struct sockaddr *)&raddr_sin, &slen)) == SOCKET_ERROR)
-        {
-            printf("recvfrom: %d\n", WSAGetLastError());
-            continue;
-        }
-
-        printf("Received packet from %s:%d\n", inet_ntoa(raddr_sin.sin_addr), ntohs(raddr_sin.sin_port));
-        dump_pkt(rbuf, recv_len);
-        rc = rusctp_header_info(rbuf, recv_len, &src_port, &dst_port, &vtag);
-        if (rc < 0)
-        {
-            continue;
-        }
-
-        rbuf_len = recv_len;
-        assoc = rusctp_accept((struct sockaddr *)&raddr_sin, slen, rbuf, &rbuf_len, sbuf, &sbuf_len, config);
-        if (assoc == NULL)
-        {
-            rbuf_off += rbuf_len;
-            if (sbuf_len > 0)
-            {
-                dump_pkt(sbuf, sbuf_len);
-                if (sendto(s, sbuf, sbuf_len, 0, (struct sockaddr *)&raddr_sin, slen) == SOCKET_ERROR)
-                {
-                    printf("sendto() failed with error code: %d", WSAGetLastError());
-                    exit(EXIT_FAILURE);
-                }
-            }
-        }
-        else
-        {
-            printf("Association established.\n");
-            rusctp_assoc_free(assoc);
-        }
-    }
-
-    closesocket(s);
-    WSACleanup();
-
-    return 0;
+    return rc;
 }
